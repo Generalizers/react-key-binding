e5821090ac2886ed325498d55649aa9d102a6232
Alan Blanchet <alan@alan-blanchet.fr>
2022-06-18 20:59:48 +0200

    [DEV]Updated react to 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,12 @@
-import { App } from './components/App';
-import { render } from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-render(<App />, document.getElementById('root'));
+import { App } from './components/App';
+
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
